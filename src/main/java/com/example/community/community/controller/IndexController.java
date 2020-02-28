993c879dbd89ada8b50efe1aff4b29c13f113af1
doTo-Y <2414811094@qq.com>
2020-02-28 18:24:58 +0800

    add index and bootstrap

---
+++ src/main/java/com/example/community/community/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String test(){
+        return "index";
+    }
+
+}
